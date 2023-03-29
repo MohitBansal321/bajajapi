7af7f1f2d288bc5c9b9ac3cbb129b7982b68060e
Mohit Bansal <78220157+MohitBansal321@users.noreply.github.com>
2023-03-29 17:13:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyparser=require('body-parser')
+const app = express()
+
+app.use(bodyparser.json())
+function generateArrays(data){
+  let even_numbers = []
+  let odd_numbers = []
+  let alphabets = []
+  let userid = "mohit_bansal_2010991291"
+  let email = "mohit1291@chitkara.edu.in"
+  let roll_number = "2010991291"
+  
+  for(const element of data){
+        if(isNaN(element)){
+          alphabets.push(element.toUpperCase())
+        }else if(Number(element) % 2 === 0){
+          even_numbers.push(element)
+        }else{
+          odd_numbers.push(element)
+        }
+  }
+  
+  return {
+    is_success: true, 
+    user_id: userid,
+    email: email,
+    roll_number: roll_number,
+    even_numbers: even_numbers, 
+    odd_numbers: odd_numbers, 
+    alphabets: alphabets
+  }
+}
+
+app.post('/process', (req, res) => {
+  const data = req.body['data']
+  const response_data = generateArrays(data)
+  res.json(response_data)
+})
+
+app.listen(3000, () => console.log('Server running at http://localhost:3000/'))
